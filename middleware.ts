405112a5181290171ad124432d99d0968cfa532d
adamyang21 <adamyang0807@hotmail.com>
2024-07-14 11:43:49 +1000

    Added sidebar components.

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+
+export default clerkMiddleware((auth, request) => {
+    const requestUrl = request.url;
+    console.log('Request URL:', requestUrl);
+
+    // Check if the request URL matches a public route
+    if (!isPublicRoute(request)) {
+        auth().protect();
+    }
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
