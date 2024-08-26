7bd5677897860e67a901a27bf450c4c09ed3a232
adamyang21 <adamyang0807@hotmail.com>
2024-08-26 11:39:15 +1000

    Added Mux video player

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,12 @@
+import Mux from "@mux/mux-node";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+
+const mux = new Mux({
+    tokenId: process.env.MUX_TOKEN_ID,
+    tokenSecret: process.env.MUX_TOKEN_SECRET
+});
 
 export async function PATCH(
     req: Request,
@@ -35,7 +41,43 @@ export async function PATCH(
             }
         });
 
-        // TODO: Handle Video Upload
+        if (values.videoUrl) {
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId
+                }
+            });
+
+            if (existingMuxData) {
+                await mux.video.assets.delete(existingMuxData.assetId);
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id
+                    }
+                });
+            }
+
+            let asset = await mux.video.assets.create({
+                input: values.videoUrl,
+                test: false
+            });
+
+            const playbackId = mux.video.assets.createPlaybackId(asset.id, {
+                policy: "public"
+            });
+
+            asset = await mux.video.assets.retrieve(asset.id);
+
+            console.log(asset.playback_ids);
+
+            await db.muxData.create({
+                data: {
+                    chapterId: params.chapterId,
+                    assetId: asset.id,
+                    playbackId: asset.playback_ids?.[0]?.id
+                }
+            });
+        }
 
         return NextResponse.json(chapter);
     }
